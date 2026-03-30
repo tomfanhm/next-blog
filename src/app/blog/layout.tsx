93e022acd02259101137430a60a9f72b1ce1c49c
Tom Fan <tomfancode@gmail.com>
2026-03-31 00:18:04 +0800

    refactor: refactor codebase

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,12 +1,5 @@
 import type { ReactNode } from "react";
 
-import { Navbar } from "@/components/navbar";
-
 export default function BlogLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="bg-background flex min-h-screen flex-col">
-      <Navbar />
-      {children}
-    </div>
-  );
+  return <div className="bg-background flex min-h-screen flex-col">{children}</div>;
 }
